4d50fe2c8ecb357235c1f08d2ee91e42deefbd2c
Kimmy Leo <kenpusney@gmail.com>
2017-07-20 19:03:06 +0800

    Add example hosted 3

---
+++ examples/hosted3.js
@@ -0,0 +1,18 @@
+
+const {request, response} = require("rec");
+
+response().contentType="text/html; charset=UTF-8";
+
+response().writer.write(
+<html>
+    <head>
+        <title>Hello</title>
+        <script type="text/javascript">
+        (function() &#x7B; alert("hello {request().getParameter("name")}"); &#x7D;)();
+        </script>
+    </head>
+    <body>
+        <h1 align="center">Hello {request().getParameter("name")}!</h1>
+    </body>
+</html>
+);
